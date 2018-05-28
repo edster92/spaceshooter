f1c6413de5ca8ebbefb5aada02f60edd323b2f86
Edik <edster@i.ua>
2018-05-28 14:10:12 +0300

    fixed laser impact

--- Assets/Scripts/Weapon scripts/WeaponLaser.cs
+++ Assets/Scripts/Weapon scripts/WeaponLaser.cs
@@ -25,10 +25,7 @@ public class WeaponLaser : BaseWeapon
 			RaycastHit hit;
 			if (Physics.Raycast (laserBeam.transform.position, laserBeam.transform.forward, out hit, 150)) 
 			{
-			//	laserBeam.transform.position = (hit.point + pos.position) / 2;
-			//	laserBeam.transform.localScale = new Vector3 (1, 1, Vector3.Magnitude (hit.point - pos.position));
 				laserBeam.SetPosition (1, (hit.point.z - laserBeam.transform.position.z) * Vector3.forward);
-
 				hit.collider.gameObject.GetComponent<BasedGameObjects> ().AddedGamage (damage [WeaponLevel - 1] * Time.deltaTime);
 				impactParticle.transform.position = laserBeam.transform.position + laserBeam.GetPosition (1);
 				if (!impactParticle.isPlaying)
