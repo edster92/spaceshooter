ba83d41ba791f8d9db96bb9ea7afa27218093f3c
Edik <edster@i.ua>
2018-06-20 14:08:37 +0300

    New text new wave

--- Assets/Scripts/Enemys/WaveController.cs
+++ Assets/Scripts/Enemys/WaveController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,10 @@ public class WaveController : MonoBehaviour {
 	Transform GOparant1;
 	[SerializeField]
 	Vector3 spawnValues;
+	[SerializeField]
+	Text nextWaveText;
+	bool txtNextLvl = false;
+	float alpha;
 
 	void Start ()
 	{
@@ -22,10 +27,12 @@ public class WaveController : MonoBehaviour {
 	{
 		for (int i = 0; i < waves.Count; i++) {
 			timeToSpawn = Time.time + Random.Range (waves [i].minTimeSpawn, waves [i].maxTimeSpawn);
+			txtNextLvl = false;
+			nextWaveText.gameObject.SetActive (false);
+
 			while (true) {
 				if (timeToSpawn <= Time.time) {
 					int U = Random.Range (0, waves [i].WaveUnits.Count - 1);
-
 					Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 					Quaternion spawnRotation = Quaternion.identity;
 					Instantiate (waves [i].WaveUnits [U].unit, spawnPosition, spawnRotation); // здесь будем вызывать спаун U юнита "waves [i].waveUnit [U].unit, отдельным класом
@@ -33,12 +40,25 @@ public class WaveController : MonoBehaviour {
 					waves [i].WaveUnits[U].unitQuantity--;
 					if (waves [i].WaveUnits [U].unitQuantity == 0)
 						waves [i].WaveUnits.RemoveAt (U);
-					if (waves [i].WaveUnits.Count == 0)
+					if (waves [i].WaveUnits.Count == 0) {
+						Debug.Log ("NEXXXT");
+						txtNextLvl = true;
+						nextWaveText.gameObject.SetActive (true);
 						break;
+					}
 					timeToSpawn = Time.time + Random.Range (waves [i].minTimeSpawn, waves [i].maxTimeSpawn);
 				}
-				yield return null;
+				yield return new WaitForSeconds (timeBetwenWaves);
 			}
+		}
+	}
+
+	void Update ()
+	{
+		if (txtNextLvl)
+		{
+			alpha += Time.deltaTime * 0.5f;
+			nextWaveText.color = new Color (1, 1, 1, alpha); 
 		}
 	}
 
