89639c0335df023503ea9e6cd1d935edc26db7f9
edster92 <edster.r92@gmail.com>
2018-06-26 23:22:51 +0300

    next wave text

--- Assets/Scripts/Enemys/WaveController.cs
+++ Assets/Scripts/Enemys/WaveController.cs
@@ -17,6 +17,7 @@ public class WaveController : MonoBehaviour {
 	Text nextWaveText;
 	bool txtNextLvl = false;
 	float alpha;
+	float timeForTxt = 3;
 
 	void Start ()
 	{
@@ -25,13 +26,20 @@ public class WaveController : MonoBehaviour {
 
 	IEnumerator wavesHandler ()
 	{
-		for (int i = 0; i < waves.Count; i++) {
+		for (int i = 0; i < waves.Count; i++) 
+		{
+			
 			timeToSpawn = Time.time + Random.Range (waves [i].minTimeSpawn, waves [i].maxTimeSpawn);
 			txtNextLvl = false;
 			nextWaveText.gameObject.SetActive (false);
 
-			while (true) {
-				if (timeToSpawn <= Time.time) {
+			while (true) 
+				
+			{
+				
+				if (timeToSpawn <= Time.time) 
+					
+				{
 					int U = Random.Range (0, waves [i].WaveUnits.Count - 1);
 					Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 					Quaternion spawnRotation = Quaternion.identity;
@@ -40,17 +48,25 @@ public class WaveController : MonoBehaviour {
 					waves [i].WaveUnits[U].unitQuantity--;
 					if (waves [i].WaveUnits [U].unitQuantity == 0)
 						waves [i].WaveUnits.RemoveAt (U);
-					if (waves [i].WaveUnits.Count == 0) {
+
+					 if (waves [i].WaveUnits.Count == 0) {
 						Debug.Log ("NEXXXT");
 						txtNextLvl = true;
 						nextWaveText.gameObject.SetActive (true);
 						break;
 					}
+
 					timeToSpawn = Time.time + Random.Range (waves [i].minTimeSpawn, waves [i].maxTimeSpawn);
 				}
+
 				yield return new WaitForSeconds (timeBetwenWaves);
 			}
 		}
+	}
+
+	public void SpawnLastWave ()
+	{
+		Debug.Log ("  LastWave!");
 	}
 
 	void Update ()
@@ -59,6 +75,11 @@ public class WaveController : MonoBehaviour {
 		{
 			alpha += Time.deltaTime * 0.5f;
 			nextWaveText.color = new Color (1, 1, 1, alpha); 
+			if (alpha > 1.5f) 
+			{
+				txtNextLvl = false;
+				nextWaveText.gameObject.SetActive (false);
+			}
 		}
 	}
 
