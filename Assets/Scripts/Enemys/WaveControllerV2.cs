8bc6521d999920113319632c911ebf065b42abb6
edster92 <edster.r92@gmail.com>
2018-12-19 14:14:09 +0200

    Script wave2Controller

---
+++ Assets/Scripts/Enemys/WaveControllerV2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveControllerV2 : MonoBehaviour {
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class waveUnit
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public waveUnit[] waves;
+    int nextWave = 0;
+    public float timeBetwenWaves = 5f;
+    public float waveCountDown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start ()
+    {
+        waveCountDown = timeBetwenWaves;
+    }
+
+    void Update ()
+    {
+        if (state != SpawnState.SPAWNING)
+        {
+            StartCoroutine(SpawnWave(waves[nextWave]));
+        }
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave (waveUnit _wave)
+    {
+        state = SpawnState.SPAWNING;
+        for (int i = 0; i<_wave.count; i++)
+        {
+
+        }
+        state = SpawnState.WAITING;
+        yield break;
+    }
+
+    void SpawnEnemy (Transform _enemy)
+    {
+        //SpawnEnemy
+        Debug.Log("Spawning enemy: " + _enemy.name);
+    }
+
+}
